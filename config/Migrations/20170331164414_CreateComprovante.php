983116eb1eb6a1772659af8174bde895297a0062
joaopaulops <joaoperreira026@gmail.com>
2017-03-31 14:15:47 -0300

    criando

---
+++ config/Migrations/20170331164414_CreateComprovante.php
@@ -0,0 +1,35 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateComprovante extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('comprovante');
+        $table->addColumn('comprovante_id', 'varchar', [
+            'default' => null,
+            'limit' => 70,
+            'null' => false,
+        ]);
+        $table->addColumn('aproved', 'boolean', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
